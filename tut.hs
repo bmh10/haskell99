ed61725c8001251d09da28b914fe06773739f46a
bmh10 <bmhomer13@gmail.com>
2016-07-17 16:47:27 +0100

    Typeclasses

--- tut.hs
+++ tut.hs
@@ -44,3 +44,26 @@ rangeInf'' = take 10 $ repeat 5
 rangeInf''' = replicate 5 10
 
 -- List Comprehension
+listComp = [x | x <- [1..10], odd x]
+
+-- Tuples
+tuple = fst (1,2) + snd (3,4)
+tupleZip = zip [1,2,3] [4,5,6]
+tupleZipInf = zip [1..] "abc"
+
+-- Typeclasses
+-- Eq => can be compared
+-- Ord => can be ordered (uses compare function)
+-- Show => can be presented as a String (show function)
+-- Read => can convert from String to another type (read function)
+-- Enum => can be sequentially ordered (e.g. can be used in list ranges)
+-- Bounded => has an upper and lower bound (functions: minBound, maxBound)
+-- Num => can act like a number
+-- Integral => whole numbers e.g. Int, Integer
+-- Floating => floating point numbers e.g. Float, Double
+
+-- fomrIntegral :: (Num b, Integral a) => a -> b
+fromIntegralEx = fromIntegral 4 + 1.2
+
+-- Syntax in Functions
+
