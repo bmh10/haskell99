dc7ea57d214cccd57bc5691170a3f177713a925e
bmh10 <bmhomer13@gmail.com>
2016-08-20 16:09:57 +0100

    surface area

--- tut.hs
+++ tut.hs
@@ -320,5 +320,10 @@ setMapAndFilterEx = Set.map toUpper $ Set.filter isLower $ Set.fromList "abcDEF"
 setNub xs = Set.toList $ Set.fromList xs
 
 -- Types & Typclasses
-data Shape = Circle Float Float Float | Rectangle Float Float Float Float
+data Shape = Circle Float Float Float | Rectangle Float Float Float Float deriving (Show)
+
+surface :: Shape -> Float
+surface (Circle _ _ r) = pi * r ^ 2
+surface (Rectangle x1 y1 x2 y2) = (abs $ x2 - x1) * (abs $ y2 -y1)
+
 
