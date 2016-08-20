0a1cc75299d454a6e7f11562bb01c5a975673271
bmh10 <bmhomer13@gmail.com>
2016-08-20 20:27:14 +0100

    Record syntax

--- tut.hs
+++ tut.hs
@@ -331,4 +331,20 @@ nudge :: Shape -> Float -> Float -> Shape
 nudge (Circle (Point x y) r) a b = Circle (Point (x+a) (y+b)) r
 nudge (Rectangle (Point x1 y1) (Point x2 y2)) a b = Rectangle (Point (x1+a) (y1+b)) (Point (x2+a) (y2+b))
 
+-- Record syntax
+data Person = Person { firstName :: String,
+                       lastName :: String,
+                       age :: Int,
+                       height :: Float,
+                       phoneNumber :: String,
+                       flavour :: String
+                     } deriving (Show)
+
+data Car = Car { company :: String,
+                 model :: String,
+                 year :: Int
+               } deriving (Show)
+
+carEx = Car {company="Ford", model="GT", year=2005}
+
 
