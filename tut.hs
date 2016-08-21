bdd48c05b273df6dbaa77e01afe49c3835916dcb
bmh10 <bmhomer13@gmail.com>
2016-08-21 20:05:56 +0100

    Vectors

--- tut.hs
+++ tut.hs
@@ -347,4 +347,15 @@ data Car = Car { company :: String,
 
 carEx = Car {company="Ford", model="GT", year=2005}
 
+-- Type params e.g. Maybe a = Just a | Nothing
 
+data Vector a = Vector a a a deriving (Show)
+
+vplus :: (Num a) => Vector a -> Vector a -> Vector a
+vplus (Vector a b c) (Vector i j k) = Vector (a+i) (b+j) (c+k)
+
+vecMult :: (Num a) => Vector a -> a -> Vector a
+vecMult (Vector a b c) n = Vector (a*n) (b*n) (c*n)
+
+scalarMult :: (Num a) => Vector a -> Vector a -> a
+scalarMult (Vector a b c) (Vector i j k) = a*i + b*j + c*k
