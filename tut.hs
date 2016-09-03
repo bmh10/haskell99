663b5b71acfadfb5ed2da652c489c93e6a878363
bmh10 <bmhomer13@gmail.com>
2016-09-03 12:18:55 +0100

    LockerLookup

--- tut.hs
+++ tut.hs
@@ -379,7 +379,7 @@ type PhoneBook = [(Name, PhoneNumber)]
 -- Parameterized type synonyms
 type AssocList k v = [(k, v)]
 
-data MyEither a b = Left a | Right b deriving (Eq, Ord, Read, Show)
+--data Either a b = Left a | Right b deriving (Eq, Ord, Read, Show)
 
 data LockerState = Taken | Free deriving (Show, Eq)
 
@@ -387,4 +387,24 @@ type Code = String
 
 type LockerMap = Map.Map Int (LockerState, Code)
 
+lockerLookup :: Int -> LockerMap -> Either String Code
+lockerLookup lockerNumber map =
+  case Map.lookup lockerNumber map of
+    Nothing -> Left $ "Locker number " ++ show lockerNumber ++ " doesn't exist."
+    Just (state, code) -> if state /= Taken 
+                        then Right code
+                        else Left $ "Locker " ++ show lockerNumber ++ " is already taken."
 
+lockers :: LockerMap  
+lockers = Map.fromList   
+    [(100,(Taken,"ZD39I"))  
+    ,(101,(Free,"JAH3I"))  
+    ,(103,(Free,"IQSA9"))  
+    ,(105,(Free,"QOTSA"))  
+    ,(109,(Taken,"893JJ"))  
+    ,(110,(Taken,"99292"))  
+    ]
+
+-- Recursive Data Structures
+
+ 
