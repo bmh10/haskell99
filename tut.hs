484be7942fec09f9e258ffd563ea24f44659455d
bmh10 <bmhomer13@gmail.com>
2016-09-04 15:39:37 +0100

    Concat list

--- tut.hs
+++ tut.hs
@@ -411,8 +411,11 @@ lockers = Map.fromList
 infixr 5 :-:
 data List a = Empty | a :-: (List a) deriving (Show, Read, Eq, Ord)
 
-infixr 5 +-+
-(+-+) :: [a] -> [a] -> [a]
-[] +-+ ys = ys
-(x:xs) +-+ ys = x : (xs ++ ys)
+infixr 5 .++
+(.++) :: List a -> List a -> List a
+Empty .++ ys = ys
+(x :-: xs) .++ ys = x :-: (xs .++ ys)
+
+
+
 
