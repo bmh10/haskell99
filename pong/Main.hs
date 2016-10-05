1a001cb151a5ee948e3a5378d77aa918a891333f
bmh10 <bmhomer13@gmail.com>
2016-10-05 16:01:57 +0100

    Draw pong game

--- pong/Main.hs
+++ pong/Main.hs
@@ -2,15 +2,45 @@ module Main where
 
 import Graphics.Gloss
 
+width, height, offset :: Int
+width = 300
+height = 300
+offset = 100
+
 window :: Display
-window = InWindow "In window" (200, 200) (10, 10)
+window = InWindow "Pong" (width, height) (offset, offset)
 
 background :: Color
-background = white
+background = black
 
 drawing :: Picture
-drawing = circle 80
+drawing = pictures [ball, 
+                    walls,
+                    mkPaddle rose 120 (-20),
+                    mkPaddle orange (-120) 40]
+  where
+    --  The pong ball.
+    ball = translate (-10) 40 $ color ballColor $ circleSolid 10
+    ballColor = dark red
 
+    --  The bottom and top walls.
+    wall :: Float -> Picture
+    wall offset =
+      translate 0 offset $
+        color wallColor $
+          rectangleSolid 270 10
+
+    wallColor = greyN 0.5
+    walls = pictures [wall 150, wall (-150)]
+
+    --  Make a paddle of a given border and vertical offset.
+    mkPaddle :: Color -> Float -> Float -> Picture
+    mkPaddle col x y = pictures
+      [ translate x y $ color col $ rectangleSolid 26 86
+      , translate x y $ color paddleColor $ rectangleSolid 20 80
+      ]
+
+    paddleColor = light (light blue)
 
 main :: IO ()
 main = display window background drawing
