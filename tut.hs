59187089cd376152fecd15043d2f9d0508ee7769
bmh10 <bmhomer13@gmail.com>
2016-11-04 22:46:07 +0000

    Command line args ex

--- tut.hs
+++ tut.hs
@@ -5,6 +5,7 @@ import Data.Char
 import Control.Monad
 import System.IO
 import System.Directory
+import System.Environment
 import qualified Data.Map as Map
 import qualified Data.Set as Set
 import qualified Geometry.Sphere as Sphere
@@ -637,4 +638,43 @@ mainFileOpsEx = do
     hClose handle  
     hClose tempHandle  
     removeFile "test.txt"  
-    renameFile tempName "test.txt" 
+    renameFile tempName "test.txt"
+
+-- Command line args
+dispatch :: [(String, [String] -> IO ())]  
+dispatch =  [ ("add", add)  
+            , ("view", view)  
+            , ("remove", remove)  
+            ] 
+
+mainCmdLineEx = do  
+    (command:args) <- getArgs  
+    let (Just action) = lookup command dispatch  
+    action args  
+ 
+add :: [String] -> IO ()  
+add [fileName, todoItem] = appendFile fileName (todoItem ++ "\n")
+
+view :: [String] -> IO ()  
+view [fileName] = do  
+    contents <- readFile fileName  
+    let todoTasks = lines contents  
+        numberedTasks = zipWith (\n line -> show n ++ " - " ++ line) [0..] todoTasks  
+    putStr $ unlines numberedTasks
+
+remove :: [String] -> IO ()  
+remove [fileName, numberString] = do  
+    handle <- openFile fileName ReadMode  
+    (tempName, tempHandle) <- openTempFile "." "temp"  
+    contents <- hGetContents handle  
+    let number = read numberString  
+        todoTasks = lines contents  
+        newTodoItems = delete (todoTasks !! number) todoTasks  
+    hPutStr tempHandle $ unlines newTodoItems  
+    hClose handle  
+    hClose tempHandle  
+    removeFile fileName  
+    renameFile tempName fileName
+
+-- Randomness
+
