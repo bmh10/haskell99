2b11948976b6b11c9e004a407bc91befe3a83310
bmh10 <bmhomer13@gmail.com>
2016-11-13 19:43:56 +0000

    Road step

--- tut.hs
+++ tut.hs
@@ -823,4 +823,18 @@ heathrowToLondon = [Section 50 10 30, Section 5 90 20, Section 40 2 25, Section 
 
 --optimalPath :: RoadSystem -> Path
 
-
+roadStep :: (Path, Path) -> Section -> (Path, Path)  
+roadStep (pathA, pathB) (Section a b c) =   
+    let priceA = sum $ map snd pathA  
+        priceB = sum $ map snd pathB  
+        forwardPriceToA = priceA + a  
+        crossPriceToA = priceB + b + c  
+        forwardPriceToB = priceB + b  
+        crossPriceToB = priceA + a + c  
+        newPathToA = if forwardPriceToA <= crossPriceToA  
+                        then (A,a):pathA  
+                        else (C,c):(B,b):pathB  
+        newPathToB = if forwardPriceToB <= crossPriceToB  
+                        then (B,b):pathB  
+                        else (C,c):(A,a):pathA  
+    in  (newPathToA, newPathToB)  
