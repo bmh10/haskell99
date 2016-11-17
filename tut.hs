d14421c36535c1e1a0830c564fd6cfed2ad2019d
bmh10 <bmhomer13@gmail.com>
2016-11-17 21:04:10 +0000

    Heathrow to London main

--- tut.hs
+++ tut.hs
@@ -851,4 +851,17 @@ groupsOf 0 _ = undefined
 groupsOf _ [] = []
 groupsOf n xs = take n xs : groupsOf n (drop n xs)
 
+mainHeathrowToLondon = do
+  contents <- getContents
+  let threes = groupsOf 3 (map read $ lines contents)
+      roadSystem = map (\[a,b,c] -> Section a b c) threes
+      path = optimalPath roadSystem
+      pathString = concat $ map (show . fst) path
+      pathPrice = sum $ map snd path
+  putStrLn $ "The best path is: " ++ pathString 
+  putStrLn $ "The price is: " ++ show pathPrice
+
+-- Functors, Applicative Functors and Monoids
+
+
 
