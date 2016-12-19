08096f95cb92400f359098f9b09f018521a40635
bmh10 <bmhomer13@gmail.com>
2016-12-19 18:46:38 +0000

    Maybe applicative def

--- tut.hs
+++ tut.hs
@@ -889,3 +889,10 @@ partialApplyEx = fmap (*) (Just 3)
 --class (Functor f) => Applicative f where  
 --    pure :: a -> f a  
 --    (<*>) :: f (a -> b) -> f a -> f b 
+
+-- maybe applicative def
+-- instance Applicative Maybe where
+--   pure = Just
+--   Nothing <*> _ = Nothing
+--   (Just f) <*> something = fmap f something
+
